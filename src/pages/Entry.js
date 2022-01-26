1916231117c8ee5f7b6a3d4224e3c46cd4001cfa
C Peterson <cpetersonmsp@gmail.com>
2022-01-25 23:56:29 -0600

    chore: styling and structure edits

--- src/pages/Entry.js
+++ src/pages/Entry.js
@@ -18,7 +18,7 @@ export default function Entry() {
 
   return (
     <div className="bg-slate-50">
-      <header className="h-fit p-16 flex justify-evenly py-16 flex-row bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <header className="h-fit w-full p-16 flex justify-evenly py-16 flex-row bg-gradient-to-r from-violet-500 to-fuchsia-500">
         <div
           className="w-2/6 flex flex-col justify-center text-slate-100"
           style={{ textShadow: "1px 1px 2px black" }}
@@ -33,13 +33,18 @@ export default function Entry() {
           </p>
         </div>
         <div className="w-1/6">
-          <SignUpForm />
+          <SignUpForm
+            handleName={handleName}
+            handlePhone={handlePhone}
+            handleEmail={handleEmail}
+            handleSubmit={handleSubmit}
+          />
         </div>
       </header>
-      <main className="px-72 mt-12">
-        <div className="cardLeftPic my-12 justify-between flex">
-          <div className="image w-4/5 mx-12 px-8 py-4 flex-col bg-slate-200 rounded-2xl">
-            <div className="meetupInfo flex h-16 bg-slate-100 justify-evenly translate-y-36 translate-x-12 rounded-2xl">
+      <main className="px-72">
+        <div className="cardLeftPic my-36 justify-between flex">
+          <div className="image shadow-lg w-4/5 px-8 py-4 mx-12 flex-col bg-slate-200 rounded-2xl">
+            <div className="meetupInfo flex h-24 bg-slate-100 justify-evenly translate-y-64 translate-x-12 rounded-2xl shadow-lg">
               <div className="infoLeft w-1/2 justify-start p-4">
                 <h5>Where</h5>
               </div>
@@ -49,47 +54,51 @@ export default function Entry() {
             </div>
           </div>
           <div className="cardText w-4/5 mx-12 px-8 py-4">
-            <h5>The Meetup</h5>
-            <h3>About the event</h3>
+            <h5 className="uppercase text-sm text-slate-600">The Meetup</h5>
+            <h3 className="text-4xl my-6">About the event</h3>
             <span>divider</span>
-            <p>
+            <p className="my-6">
               Disrupt direct trade chicharrones chia biodiesel 90's celiac
               hoodie austin plaid yuccie pug blog. Mustache umami marfa kale
               chips mixtape swag seitan 90's prism.
             </p>
-            <button>Join Us</button>
+            <button className="border-2 rounded-lg text-white bg-pink-500 w-40 h-12 shadow-lg">
+              Join Us
+            </button>
           </div>
         </div>
-        <div className="cardRightPic justify-between my-12 flex">
-          <div className="cardText mx-12 w-4/5 px-8 py-4">
-            <h5>About Us</h5>
-            <h3>What We're Doing</h3>
+        <div className="cardRightPic justify-between my-36 flex">
+          <div className="cardText w-4/5 mx-12 px-8 py-4">
+            <h5 className="uppercase text-sm text-slate-600">About Us</h5>
+            <h3 className="text-4xl my-6">What We're Doing</h3>
             <span>divider</span>
-            <p>
+            <p className="my-6">
               Cardigan fashion axe intelligentsia stumptown drinking vinegar
               schlitz shaman squid chicharrones knausgaard. Dreamcatcher iceland
               master cleanse paleo gentrify, distillery lyft pug gluten-free
               echo park.
             </p>
-            <button>Join Us</button>
+            <button className="border-2 rounded-lg text-white bg-pink-500 w-40 h-12 shadow-lg">
+              Join Us
+            </button>
           </div>
-          <div className="doubleImage mx-12 w-4/5 px-8 py-4">
-            <p className="bg-slate-200 h-full rounded-2xl">image</p>
-          </div>
+          <div className="image shadow-lg w-4/5 px-8 py-4 mx-12 flex-col bg-slate-200 rounded-2xl"></div>
         </div>
-        <div className="cardLeftPic justify-between my-12 flex">
-          <div className="image w-4/5 px-8 py-4 mx-12 flex-col bg-slate-200 rounded-2xl"></div>
+        <div className="cardLeftPic justify-between my-36 flex">
+          <div className="image shadow-lg w-4/5 px-8 py-4 mx-12 flex-col bg-slate-200 rounded-2xl"></div>
           <div className="cardText w-4/5 mx-12 px-8 py-4">
-            <h5>The Meetup</h5>
-            <h3>About the event</h3>
+            <h5 className="uppercase text-sm text-slate-600">The Meetup</h5>
+            <h3 className="text-4xl my-6">About the event</h3>
             <span>divider</span>
-            <p>
+            <p className="my-6">
               Health goth meggings activated charcoal, fam ethical stumptown
               selfies small batch air plant poke slow-carb palo santo VHS
               live-edge pickled. Locavore plaid PBR&B, forage flannel gastropub
               literally wayfarers try-hard.
             </p>
-            <button>Join Us</button>
+            <button className="border-2 rounded-lg text-white bg-pink-500 w-40 h-12 shadow-lg">
+              Join Us
+            </button>
           </div>
         </div>
       </main>
